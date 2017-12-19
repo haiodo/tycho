a5002bff2f4496901304b195e79d3e62f9c5f77e
Andrey Sobolev <haiodo@gmail.com>
2017-12-19 17:35:22 +0700

    Include all deps to repository

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ rm -rf ./repository
 # Do a build
 # 
 mvn clean deploy -DaltDeploymentRepository=snapshot-repo::default::file:./repository
+mvn clean deploy -Preleng -f tycho-releng/pom.xml
 mvn clean deploy -f ../tycho.extras -DaltDeploymentRepository=snapshot-repo::default::file:./repository
