fcb6643c9b71ac5929731787a506a36345485d7a
Andrey Sobolev <haiodo@gmail.com>
2018-04-05 15:21:05 +0700

    Publish 1.2.0.han version of fast tycho

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 rm -rf ./repository
 
 # Do a build
-# 
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion="1.2.0.han"
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion="1.2.0.han" -f ../tycho.extras
+
 mvn clean deploy -DaltDeploymentRepository=snapshot-repo::default::file:./repository
 mvn clean deploy -Preleng -f tycho-releng/pom.xml
 mvn clean deploy -f ../tycho.extras -DaltDeploymentRepository=snapshot-repo::default::file:./repository
+
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion="1.2.0-SNAPSHOT"
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion="1.2.0-SNAPSHOT" -f ../tycho.extras
