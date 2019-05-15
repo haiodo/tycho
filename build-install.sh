0befe52ea68720ddb306ce3c2515a52c52986d18
Andrey Sobolev <haiodo@gmail.com>
2019-05-15 22:01:18 +0700

    Few fixes

---
+++ build-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf ./repository
+
+# Do a build
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:1.0.0:set-version -DnewVersion="1.4.0.han1"
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:1.0.0:set-version -DnewVersion="1.4.0.han1" -f ../tycho.extras
+
+mvn clean install
+mvn clean install -Preleng -f tycho-releng/pom.xml
+mvn clean install -f ../tycho.extras
+
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:1.0.0:set-version -DnewVersion="1.4.0-SNAPSHOT"
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:1.0.0:set-version -DnewVersion="1.4.0" -f ../tycho.extras
