141573bbcf1ee2d1e62b83c12da4dda17db98c4f
Andrey Sobolev <haiodo@gmail.com>
2019-05-16 00:00:25 +0700

    revert versions

--- revert_versions.sh
+++ revert_versions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:1.0.0:set-version -DnewVersion="1.4.0-SNAPSHOT"
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:1.0.0:set-version -DnewVersion="1.4.0-SNAPSHOT" -f ../tycho.extras
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:1.0.0:set-version -DnewVersion="1.4.0"
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:1.0.0:set-version -DnewVersion="1.4.0" -f ../tycho.extras
